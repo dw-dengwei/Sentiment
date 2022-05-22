564f037e6ce922097163fa8245b128c5b8ceb9f1
Wei Deng <dw-dengwei@outlook.com>
2022-05-22 19:42:18 +0800

    和后端交互完成

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,6 +5,11 @@
 #include <QFileDialog>
 #include <QMenu>
 #include <QDebug>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QNetworkAccessManager>
+#include <QHttpPart>
+#include <string>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -61,7 +66,63 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_predict_clicked()
 {
-    // TODO
-    int ret = 2;
+    ui->predict->setIcon(QIcon(":/img/unknown.png"));
+    QHttpMultiPart *multiPart = new QHttpMultiPart(QHttpMultiPart::FormDataType);
+    QHttpPart imagePart, textPart;
+    textPart.setHeader(QNetworkRequest::ContentDispositionHeader, QVariant("form-data; name=\"text\""));
+    bool flag = false;
+    QString path;
+    for(int i = 0; i < 4; i++)
+    {
+        if(img[i]->has_img)
+        {
+            path = img[i]->img_path;
+            flag = true;
+            imagePart.setHeader(
+                        QNetworkRequest::ContentDispositionHeader,
+                        QVariant(QString("form-data; name=\"%1\"; filename=\"%2\"")\
+                                 .arg("image")\
+                                 .arg(QFileInfo(path).fileName()))
+                        );
+            break;
+        }
+    }
+
+    if(!flag)
+    {
+        qDebug() << "No Image Error!";
+        return;
+    }
+
+    textPart.setBody(ui->textInput->toPlainText().toStdString().data());
+    QFile *file = new QFile(path);
+    file->open(QIODevice::ReadOnly);
+    imagePart.setBodyDevice(file);          //方便后续内存释放
+    file->setParent(multiPart); // we cannot delete the file now, so delete it with the multiPart
+    imagePart.setHeader(QNetworkRequest::ContentTypeHeader, "image/png");
+
+    multiPart->append(imagePart);
+    multiPart->append(textPart);
+
+    QNetworkRequest networkRequest;
+    networkRequest.setUrl(QUrl("http://remote:12333/"));
+
+    //发送请求
+    QEventLoop loop;
+    QNetworkAccessManager manager;
+    QObject::connect(&manager, SIGNAL(finished(QNetworkReply*)), &loop, SLOT(quit()));
+
+    QNetworkReply *reply = manager.post(networkRequest, multiPart);
+    multiPart->setParent(reply); // delete the multiPart with the reply
+    loop.exec();
+    if(reply->error() != QNetworkReply::NoError)
+    {
+        qDebug() << "Error: " << reply->error();
+        return;
+    }
+    int data = reply->readAll().toStdString()[0] - '0';
+    qDebug() << data;
+
+    int ret = data;
     ui->predict->setIcon(QIcon(status_map[ret]));
 }
